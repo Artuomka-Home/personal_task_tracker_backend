926d32c022f23389bc56dba2338e13822be7dc06
TanyaLark <tanyaenlang9898@gmail.com>
2023-09-18 18:25:49 +0300

    Added express-rate-limit configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { apiLimiter } from './middlewares/express-rete-limit-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+ // Apply the rate limiting middleware to API calls only
+app.use('/user', apiLimiter)
 }
 bootstrap();
