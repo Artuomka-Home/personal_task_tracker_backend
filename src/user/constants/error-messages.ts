070e6dbf821eba1937225f4923790d42b2dc95ba
TanyaLark <tanyaenlang9898@gmail.com>
2023-10-14 14:59:14 +0300

    Reworked user e2e tests. Added data faker.

---
+++ src/user/constants/error-messages.ts
@@ -0,0 +1,4 @@
+export const errorMessages = {
+    DUPLICATE_NAME: (name: string) => `User name ${name} already exists`,
+    DUPLICATE_EMAIL: (email: string) => `User email ${email} already exists`,
+};
--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
-import { sign } from 'jsonwebtoken';
+import { AppModule } from '../src/app.module';
+import { errorMessages } from '../src/user/constants/error-messages';
+import { createValidUserInput } from './faker/user-fake-data';
+import { getJwtToken } from './utils/get-jwt-token';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
-  let validToken: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,28 +16,106 @@ describe('UserController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-
-    validToken = sign({ id: '1' }, process.env.JWT_SECRET_KEY);
   });
 
   afterAll(async () => {
     await app.close();
   });
 
+  it('should register a user when valid input is provided', async () => {
+    const validUserInput = createValidUserInput();
+
+    const response = await request(app.getHttpServer())
+      .post('/user/register')
+      .send(validUserInput)
+      .expect(HttpStatus.CREATED);
+
+    const registerUserResponse = {
+      id: response.body.id,
+      name: validUserInput.name,
+      email: validUserInput.email,
+      created_at: response.body.created_at,
+      updated_at: response.body.updated_at,
+    };
+
+    expect(response.body).toEqual(registerUserResponse);
+
+    // Send a POST request to the register endpoint with duplicate name
+    const responseDuplicateName = await request(app.getHttpServer())
+      .post('/user/register')
+      .send(validUserInput)
+      .expect(HttpStatus.BAD_REQUEST);
+
+    expect(responseDuplicateName.body.message).toContain(errorMessages.DUPLICATE_NAME(validUserInput.name));
+
+    // Send a POST request to the register endpoint with duplicate email
+    validUserInput.name = 'TestUser2';
+    const responseDuplicateEmail = await request(app.getHttpServer())
+      .post('/user/register')
+      .send(validUserInput)
+      .expect(HttpStatus.BAD_REQUEST);
+
+    expect(responseDuplicateEmail.body.message).toContain(errorMessages.DUPLICATE_EMAIL(validUserInput.email));
+
+    const validToken = getJwtToken(registerUserResponse.id);
+    await request(app.getHttpServer())
+      .delete(`/user/${registerUserResponse.id}`)
+      .set('Authorization', `Bearer ${validToken}`)
+      .expect(HttpStatus.OK);
+  });
+
+  it('should successfully login a user with valid credentials', async () => {
+    const validUserInput = createValidUserInput();
+    const registerResponse = await request(app.getHttpServer())
+      .post('/user/register')
+      .send(validUserInput)
+      .expect(HttpStatus.CREATED);
+
+    const validLoginDto = {
+      email: registerResponse.body.email,
+      password: validUserInput.password,
+    };
+
+    const response = await request(app.getHttpServer()).post('/user/login').send(validLoginDto).expect(HttpStatus.OK);
+
+    expect(response.body).toHaveProperty('token');
+    expect(response.body).toHaveProperty('exp');
+
+    const validToken = getJwtToken(registerResponse.body.id);
+    await request(app.getHttpServer())
+      .delete(`/user/${registerResponse.body.id}`)
+      .set('Authorization', `Bearer ${validToken}`)
+      .expect(HttpStatus.OK);
+  });
+
+  it('should return a 404 status for an invalid user', async () => {
+    const invalidLoginDto = {
+      email: 'nonexistent@example.com',
+      password: 'invalidPassword',
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/user/login')
+      .send(invalidLoginDto)
+      .expect(HttpStatus.NOT_FOUND);
+  });
+
   it('should return a user when an user is authenticated', async () => {
-    const userId = 1;
-    const name = 'Yagor';
-    const email = 'testYagor@gmail.com';
-    const created_at = '2023-10-09T12:58:07.964Z';
-    const updated_at = '2023-10-09T12:58:07.964Z';
+    const validUserInput = createValidUserInput();
+    const registerUserResponse = await request(app.getHttpServer())
+      .post('/user/register')
+      .send(validUserInput)
+      .expect(HttpStatus.CREATED);
 
     const expectedUser = {
-      id: userId,
-      name,
-      email,
-      created_at,
-      updated_at,
+      id: registerUserResponse.body.id,
+      name: registerUserResponse.body.name,
+      email: registerUserResponse.body.email,
+      created_at: registerUserResponse.body.created_at,
+      updated_at: registerUserResponse.body.updated_at,
     };
+
+    const validToken = getJwtToken(expectedUser.id);
 
     const response = await request(app.getHttpServer())
       .get(`/user`)
@@ -44,6 +123,11 @@ describe('UserController (e2e)', () => {
       .expect(HttpStatus.OK);
 
     expect(response.body).toEqual(expectedUser);
+
+    await request(app.getHttpServer())
+      .delete(`/user/${expectedUser.id}`)
+      .set('Authorization', `Bearer ${validToken}`)
+      .expect(HttpStatus.OK);
   });
 
   it('should return a 401 error when an user isn`t authenticated', async () => {
